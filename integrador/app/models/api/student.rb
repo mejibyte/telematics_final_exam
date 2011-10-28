cd9d36fce0600e13400be11b23642963babe2413
Andrés Mejía <andmej@gmail.com>
2011-10-27 21:30:16 -0500

    Little tweaks

--- integrador/app/models/api/student.rb
+++ integrador/app/models/api/student.rb
@@ -3,7 +3,7 @@ class Api::Student < Api::Base
   def self.all(token, course_id, group)
     return [] if token.blank? or course_id.blank? or group.blank?
     request = get("/ei/courses/#{course_id}/students", :query => { :token => token, :group => group })
-    [request["ei"]["curso"]["estudiante"]].flatten
+    [request["ei"]["curso"]["estudiante"]].flatten.compact
   end
   
   def self.search(token, name_pattern)
@@ -17,7 +17,7 @@ class Api::Student < Api::Base
     return nil if token.blank? or student_id.blank?
     request = get("/ar/students/#{student_id}", :query => { :token => token, :student_id => student_id })
     student = [request["ar"]["estudiante"]].flatten.first
-    student["cursos"]["curso"] = [student["cursos"]["curso"]] unless student["cursos"]["curso"].is_a?(Array)
+    student["cursos"]["curso"] = [student["cursos"]["curso"]].flatten.compact
     student
   end
 end
